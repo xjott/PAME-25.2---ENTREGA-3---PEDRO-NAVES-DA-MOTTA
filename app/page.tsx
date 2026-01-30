bcbadb96188f75e0e1b67d77e3c9d506fe6eefcb
Pedro Motta <163004710+xjott@users.noreply.github.com>
2026-01-30 14:30:57 -0300

    msg

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,25 @@
 import Link from "next/link";
 import { eventos } from "../eventos";
-
+// primeira coisa feita no return da função: o banner
+// 
 export default function Home(){
     var destaques = eventos.filter((evento) => evento.destaque);
     var proximos = eventos.slice(0,3);
-// pelo commit
+
+    return(
+        <div className="page">
+            <section className="banner">
+            <div className="titulo-do-banner">Destaques da Semana</div>
+
+            <div className="destaques">
+            {destaques.map((evento)=> (
+                <Link key={evento.id} className="destaqueCard" href={`/eventos/${evento.id}`}>
+                    <img className="cardimg" src={evento.imagem} alt={evento.nome}></img>
+                    <div className="cardbody">
+                    <div className="cardtitle">
+                </Link>
+            ))}
+            </section>
+        </div>
+    )
 }
