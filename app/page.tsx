050868f64c021dca08f25dc9581111d50952f6e4
Pedro Motta <163004710+xjott@users.noreply.github.com>
2026-01-30 15:18:53 -0300

    msg

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { eventos } from "../eventos";
 // primeira coisa feita no return da função: o banner
 // 
 export default function Home(){
-    var destaques = eventos.filter((evento) => evento.destaque);
+    var destaques = eventos.filter((evento: any) => evento.destaque);
     var proximos = eventos.slice(0,3);
 
     return(
@@ -12,13 +12,17 @@ export default function Home(){
             <div className="titulo-do-banner">Destaques da Semana</div>
 
             <div className="destaques">
-            {destaques.map((evento)=> (
+            {destaques.map((evento: any)=> (
                 <Link key={evento.id} className="destaqueCard" href={`/eventos/${evento.id}`}>
                     <img className="cardimg" src={evento.imagem} alt={evento.nome}></img>
                     <div className="cardbody">
-                    <div className="cardtitle">
+                    <div className="cardtitle">{evento.nome}</div>
+                    <div className="cardmeta">{evento.data} {evento.horario}</div>
+                    </div>
+                    <div className="cardprice">A partir de R$ {evento.valorInicial}</div>
                 </Link>
             ))}
+            </div>
             </section>
         </div>
     )
