5c080233f78e287d91e86982fe9858401fb1c1c1
Pedro Motta <163004710+xjott@users.noreply.github.com>
2026-01-31 22:25:43 -0300

    homePage

--- app/page.tsx
+++ app/page.tsx
@@ -7,19 +7,58 @@ export default function HomePage(){
     const destaques = eventos.filter((evento)=> evento.destaque);
     const proximos = eventos.slice(0,3);
 
-    return(
-        <div className="pagina">
-            <section className="banner">
-                <div className="tituloBanner">
-                    Destaques da Semana
+    //preciso fazer duas coisas na home, a seção de destaques e a sessão de próximos eventos
+
+    // section e depois div nas seções
+    
+      return (
+    <div className="page">
+      <section className="banner">
+        <div className="bannertitle">Destaques da Semana</div>
+        <div className="destaques">
+          {destaques.map((evento) => (
+            <Link key={evento.id} className="destaqueCard" href={`/eventos/${evento.id}`}>
+              <img className="cardimg" src={evento.imagem} alt={evento.nome} />
+              <div className="cardbody">
+                <div className="cardtitle">{evento.nome}</div>
+                <div className="cardmeta">
+                  {evento.data} • {evento.horario}
                 </div>
-                <div className="destaques">
-                    {destaques.map((evento)=>(
-                        
-                    ))}
-                </div>
-            </section>
+                <div className="cardprice">A partir de R$ {evento.valorInicial}</div>
+              </div>
+            </Link>
+                  ))}
         </div>
-    )
+      </section>
+
+      {/* primeiro uma div geral, depois uma seção pras duas e por fim colocar os detalhes do folder em si, fiz isso tanto para os destaques da semana quanto para os próximos eventos */}
+
+      <section className="section">
+        <div className="sectiontitle">Próximos Eventos</div>
+
+        <div className="grid">
+          {proximos.map((evento) => (
+            <Link key={evento.id} className="eventCard" href={`/eventos/${evento.id}`}>
+              <img className="cardimg" src={evento.imagem} alt={evento.nome} />
+              <div className="cardbody">
+                <div className="cardtitle">{evento.nome}</div>
+                <div className="cardmeta">
+                  {evento.data} • {evento.horario}
+                </div>
+                <div className="cardprice">A partir de R$ {evento.valorInicial}</div>
+              </div>
+            </Link>
+          ))}
+                </div>
+      </section>
+
+      {/* por fim, fazer um footer pra página */}
+
+      <footer className="footer">
+        <div>Contato: contato@cristinaeventos.com</div>
+        <div>Localização: Av. Fluxo, 456 — Rio de Janeiro</div>
+      </footer>
+    </div>
+  );
 }
 
